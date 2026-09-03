5df4b0fed425d87ef6fe0bd2285476f8e4c696fc
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-03 13:51:45 +0900

    [Passed] 두 번의 연산 (91ms, 9MB)

---
+++ trail1/두 번의 연산/two-operations.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if (a%2==1) a+=3;
+        if (a%3==0) a/=3;
+        System.out.println(a);
+    }
+}
