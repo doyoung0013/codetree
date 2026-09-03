a390a12907e6c06c241a00fa27bc23702a48bc16
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-03 09:16:31 +0900

    [Passed] 실수 받아 그대로 출력 (98ms, 9MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double N = Double.parseDouble(br.readLine());
+        System.out.printf("%.2f", N);
+
+    }
+}
