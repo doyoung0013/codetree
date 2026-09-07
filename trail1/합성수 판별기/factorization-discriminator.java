d1e94de741bc0bef8c218e0dda343ac06634ef91
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-07 17:32:43 +0900

    [Passed] 합성수 판별기 (87ms, 9MB)

---
+++ trail1/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean isSyn = false;
+        for (int i=2; i<n; i++){
+            if(n%i == 0){
+                isSyn = true;
+                break;
+            }
+        }
+
+        if(isSyn) System.out.println('C');
+        else System.out.println('N');
+    }
+}
