e64db683276859ad396539c11740fd5c47a570bb
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-07 17:09:33 +0900

    [Passed] 윤년 횟수 구하기 (92ms, 9MB)

---
+++ trail1/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i=1; i<n+1; i++){
+            if (i % 100 == 0 && i % 400 != 0) continue;
+            if (i % 4 == 0) result++;
+        }
+
+        System.out.print(result);
+    }
+}
