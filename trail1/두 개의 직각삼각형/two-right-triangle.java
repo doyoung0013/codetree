66ff8feef6901fec349197caf60057b9336b1553
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-16 14:45:37 +0900

    [Passed] 두 개의 직각삼각형 (91ms, 9MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n-i; j > 0; j--) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 2*i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n-i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
