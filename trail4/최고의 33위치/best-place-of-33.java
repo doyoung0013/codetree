db798c5d5843ce060c7fb475ed86750894dc01aa
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-16 18:32:31 +0900

    [Passed] 최고의 33위치 (136ms, 10MB)

---
+++ trail4/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+        int max_v = 0;
+        for(int i=0; i<n-2; i++){
+            for(int j=0; j<n-2; j++){
+                int v = 0;
+                v += (grid[i][j] + grid[i][j+1] + grid[i][j+2] + grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2] + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]);
+                if (v > max_v) max_v = v;
+            }
+        }
+
+        System.out.println(max_v);
+    }
+}
