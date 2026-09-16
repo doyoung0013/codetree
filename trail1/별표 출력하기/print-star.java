1c4aa143a6e943a9c261229cd4f4878f7c8b9d62
doyoung0013 <163370991+doyoung0013@users.noreply.github.com>
2026-09-16 12:01:40 +0900

    [Passed] 별표 출력하기 (96ms, 9MB)

---
+++ trail1/별표 출력하기/print-star.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i=n-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
